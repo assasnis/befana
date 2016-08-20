66959c47a01d39a55ca8871abd4999968a0cd0e0
assasnis <carlos.cancino.93@hotmail.com>
2016-08-20 14:18:18 -0300

    .

--- application/views/Usuarios/index_user.php
+++ application/views/Usuarios/index_user.php
@@ -117,10 +117,7 @@
             foreach ($pagos_clientes->result() as $pago_actual) {
 
                 $producto=$pago_actual->producto;
-<<<<<<< HEAD
                
-=======
->>>>>>> origin/master
                 $detail=$this->User_model->detalles_producto($producto);
                 foreach ($detail->result() as $detalle) {
                
