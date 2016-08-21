90c757812953fe518dfe468838968a2636a957b5
assasnis <carlos.cancino.93@hotmail.com>
2016-08-21 01:20:44 -0300

    Update orden_compra.php

--- application/views/Usuarios/orden_compra.php
+++ application/views/Usuarios/orden_compra.php
@@ -4,7 +4,13 @@ $user_data = $this->session->userdata('userdata');
 
 ?><!DOCTYPE html>
 
+
+
+
+
     <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+
+
     <div class="panel panel-info">
       <div class="panel-heading">
         <h1 class="panel-title"><h3> <span class="glyphicon glyphicon-ok" aria-hidden="true"> </span>
@@ -21,6 +27,7 @@ $user_data = $this->session->userdata('userdata');
           echo '<thead>';
             echo '<tr>';
               echo '<th> <span class="glyphicon glyphicon-envelope" aria-hidden="true"> </span> &nbsp Producto</th>';
+              echo '<th> <span class="glyphicon glyphicon-registration-mark" aria-hidden="true"> </span> &nbsp Marca</th>';
               echo '<th> <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"> </span> &nbsp Proveedor</th>';              
               echo '<th> <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"> </span> &nbsp Cantidad</th>';
               echo '<th> <span class="glyphicon glyphicon-object-align-vertical" aria-hidden="true"> </span> &nbsp Peso Unitario(g)</th>';     
@@ -36,6 +43,7 @@ $user_data = $this->session->userdata('userdata');
           foreach ($orden->result() as $ordenes) {
               echo '<tr>';
               echo '<td>'.$ordenes->producto.'</td>';
+              echo '<td>'.$ordenes->marca.'</td>';
               echo '<td>'.$ordenes->proveedor.'</td>';
               echo '<td>'.$ordenes->cantidad.'</td>';
               echo '<td>'.$ordenes->peso.'</td>';
@@ -43,7 +51,7 @@ $user_data = $this->session->userdata('userdata');
 
               
               
-              $url_aprobar = base_url() . 'index.php/Gerente_control/pago_aprobado?id='.$ordenes->producto;
+              $url_aprobar = base_url() . 'index.php/user_controller/aprobar_orden_compra?id='.$ordenes->id;
               echo '<td><a href= "#" class="btn btn-success" data-toggle="modal" data-target="#modal_aprobar" role="button">Aprobar</a></td>';
 
               echo '<div class="modal fade" id="modal_aprobar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">';
@@ -54,11 +62,11 @@ $user_data = $this->session->userdata('userdata');
               echo '        <h4 class="modal-title" id="myModalLabel">Confirmación de aprobación</h4>';
               echo '      </div>';
               echo '      <div class="modal-body">';
-              echo ' ¿Esta seguro que desea aprobar el pago?';
+              echo ' ¿Esta seguro que desea aprobar la orden?';
               echo '      </div>';
               echo '      <div class="modal-footer">';
               echo '        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>';
-              echo '        <a href="' . $url_aprobar . '" "#" class="btn btn-success" role="button">Aprobar pago</a>';
+              echo '        <a href="' . $url_aprobar . '" "#" class="btn btn-success" role="button">Aprobar compra</a>';
               echo '      </div>';
               echo '    </div>';
               echo '  </div>';
@@ -74,7 +82,11 @@ $user_data = $this->session->userdata('userdata');
       </div>
     </div>
   </div>
+
+
   </div>
+
+
 
 </body>
 </html>
