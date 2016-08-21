33972dc2e4fe134c51f8b3d5eaa9a2564aef3f9f
assasnis <carlos.cancino.93@hotmail.com>
2016-08-21 00:51:52 -0300

    nuevas funciones

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -65,6 +65,68 @@ class User_model extends CI_Model
 			}
 	}
 
+	function ordenes_compra()
+	{
+		$consulta="SELECT * FROM orden_compra";
+			$respuesta=$this->db->query($consulta);
+			if($respuesta){ 
+				return $respuesta;				
+			}		
+			else{
+			 
+			 	return false;
+			}
+	}
+
+	function id_producto($nombre,$marca,$peso)
+	{
+		
+		$this->db->select('*');
+		$this->db->where('peso',$peso);
+		$this->db->like('marca',$marca);
+		$this->db->like('nombre', $nombre);
+		$query=$this->db->get("productos");		
+		if(count($query))
+		{
+			return $query;
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
+
+	function nuevo_producto($array){
+		
+		$this->db->insert('productos', $array);
+	}
+
+	function aceptar_compra($array){
+		
+		$this->db->insert('compras', $array);		
+	}
+	function borrar_orden($id_orden)
+	{
+		$this->db->where('id', $id_orden);
+		$this->db->delete('orden_compra');
+	}
+
+	function selec_orden($id)
+	{
+		$consulta="SELECT * FROM orden_compra where md5(id)='".md5($id)."'";
+			$respuesta=$this->db->query($consulta);
+			if($respuesta){ 
+				return $respuesta;				
+			}		
+			else{
+			 
+			 	return false;
+			}
+
+
+	}
+
+
 }
 
 
